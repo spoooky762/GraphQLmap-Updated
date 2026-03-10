dd5de5ecbee5cf83fa90d82fc30ebd66869a74ea
not_k4sp3r <145744192+spoooky762@users.noreply.github.com>
2026-03-10 13:52:09 +0800

    Refactor utils.py to improve readability and structure
    added first part of a open pull request from the original repository

--- graphqlmap/utils.py
+++ graphqlmap/utils.py
@@ -54,7 +54,7 @@ def requester(url, method, payload, proxy, headers=None, use_json=False, is_batc
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', action='store', dest='url', help="URL to query : example.com/graphql?query={}")
+    parser.add_argument('-u', action='store', dest='url', help="URL to query : https://example.com/graphql?query={}")
     parser.add_argument('-v', action='store', dest='verbosity', help="Enable verbosity", nargs='?', const=True)
     parser.add_argument('--method', action='store', dest='method',
                         help="HTTP Method to use interact with /graphql endpoint", nargs='?', const=True, default="GET")
@@ -79,4 +79,5 @@ def display_help():
     print("[+] \033[92mmysqli      \033[0m: exploit a sql injection inside a GraphQL query")
     print("[+] \033[92mmssqli      \033[0m: exploit a sql injection inside a GraphQL query")
     print("[+] \033[92mexit        \033[0m: gracefully exit the application")
+
 
